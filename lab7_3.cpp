fa0d873ae662c8f89ff630adece7c58b65b919a9
ratchapon prangtong <ppete2547@gmail.com>
2022-12-13 16:52:59 +0700

    lab7_3.cpp fish

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,11 +1,35 @@
 #include<iostream>
-
+#include<cmath>
 using namespace std;
+int c;
+int adiff(int x, int y){
+ x = x%360;
+ y = y%360;
+  if(x >= y){
+   c = x-y;
+  }
+   if(y >= x){
+    c = y-x;
+  }
+if(c > 180){
+  c = 360 - c;
+}
+return c;
+
+
+
+
+}
+
+
+
+
+}
 
 
 int main(){
   cout << adiff(180,270);
-  cout << adiff(210,45);
+cout << adiff(210,45);
   cout << adiff(0,360);
   cout << adiff(10,350);
   cout << adiff(95,260);
