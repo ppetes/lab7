3e6755e26bd89245bb73ed83c4897cb1a3ecd12f
ratchapon prangtong <ppete2547@gmail.com>
2022-12-13 15:50:21 +0700

    lab7_4.cpp fish

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -10,6 +10,20 @@ int main(){
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
 
     //Write your code here.
+	double x;
+	int i = 0;
+	cout <<"Please input GPA: ";
+	cin >> x;
+	while(i < 10){
+		if(gpa[i] >= x){
+			cout << id[i] << " " << name[i] << " " << gpa[i] << endl;
+		}
+		
+			
+		i++;
+	}
+
+
 		
 	return 0;
 }
